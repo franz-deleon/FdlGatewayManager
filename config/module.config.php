4c7777ebdff31ef7821f7a8229c13d42baabf0d5
franz de leon <franz_dl@yahoo.com>
2013-10-25 06:05:15 +0000

    initial files for feature 2.0

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,45 @@ return array(
         'factories' => array(
             'FdlGatewayPlugin' => 'FdlGatewayManager\Service\FdlGatewayPluginFactory',
         ),
+        'abstract_factories' => array(
+            'FdlGatewayManager\Factory\TableAbstractFactory',
+        ),
     ),
+    'fdl_gateway_manager_config' => array(
+        /**
+         * These are the parameters of Zend\Db\TableGateway\TableGateway.
+         * Each parameters are invoked in default by their corresponding
+         * Abstract Factory class for their default values.
+         *
+         * To implement your own class, simply override the values and
+         * use it as your service manager key name
+         *
+         * Example:
+         * <code>
+         *     factories => array('FdlTableGateway\Table' => function ($sm) {})
+         * </code>
+         */
+        'table_gateway' => array(
+            'table'    => 'FdlTableGateway\Table',
+            'adapter'  => 'FdlTableGateway\Adapter',
+            'features' => 'FdlTableGateway\Features',
+            'result_set_prototype' => 'FdlTableGateway\ResultSetPrototype',
+            'sql'      => 'FdlTableGateway\Sql',
+        ),
+        /**
+         * These is where to store the table and entity mapping classes.
+         * Values are namespaces
+         */
+        'asset_location' => array(
+            'tables'   => '', // to be filled in implementing module
+            'entities' => '', // to be filled in implementing module
+        ),
+        /**
+         * Default classes to use for TableGateways and Adapter
+         */
+        'gateway' => 'Zend\Db\TableGateway\TableGateway',
+        'adapter' => 'Zend\Db\Adapter\Adapter',
+     ),
     'fdl_gateway_manager' => array(
         'gateway' => 'Zend\Db\TableGateway\TableGateway',
         'adapter' => 'Zend\Db\Adapter\Adapter',
---
+++ src/FdlGatewayManager/Factory/TableAbstractFactory.php
@@ -0,0 +1,36 @@
+<?php
+namespace FdlGatewayManager\Factory;
+
+use Zend\ServiceManager;
+
+class TableAbstractFactory implements ServiceManager\AbstractFactoryInterface
+{
+    /**
+     * Determine if we can create a service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return bool
+     */
+    public function canCreateServiceWithName(ServiceManager\ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        $config = $serviceLocator->get('config');
+        $serviceName = $config['fdl_gateway_manager_config']['table_gateway']['table'];
+
+        return ($requestedName === $serviceName);
+    }
+
+    /**
+     * Create service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return mixed
+    */
+    public function createServiceWithName(ServiceManager\ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        print "har";die;
+    }
+}
